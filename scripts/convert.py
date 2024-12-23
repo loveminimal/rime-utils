d3fd6e5b0e0c94215f8801c099ff639dfd3c97a4
jack <loveminimal@163.com>
2024-12-23 10:14:32 +0800

    feat: some updates

---
+++ scripts/convert.py
@@ -0,0 +1,50 @@
+import os
+import shutil
+from pathlib import Path
+# from scripts.old.header import get_header
+
+
+def convert(SRC_DIR, OUT_DIR, FILE_ENDSWITH_FILETER):
+	# 遍历源文件夹文件，处理
+	for file_path in SRC_DIR.iterdir():
+		if file_path.is_file():
+			file_name = file_path.name
+			# print(file_name)
+			if not file_name.endswith(FILE_ENDSWITH_FILETER):
+				continue
+
+			src_file_path = SRC_DIR / file_name
+			out_file_path = OUT_DIR / file_name
+			
+			# 添加词库头
+			# with open(out_file_path, 'a', encoding='utf-8') as o:
+			#     o.write(get_header(file_name))
+
+
+			with open(src_file_path, 'r', encoding='utf-8') as f:
+				num = 0
+				res = ''
+				for line in f.readlines():
+					if not line.startswith(('#', ' ', '\n')):    # 忽略非词表行
+						line_arr = line.strip().split('\t')
+						# print(len(line_arr[1]))
+						# if (len(line_arr[0]) == 1 and ('\u4e00' <= line_arr[0][:1] <= '\u9fff')):
+						if (len(line_arr[0]) == 1):
+							# print(f'{line_arr[1]}\t{line_arr[0]}')
+							# res = res + line_arr[1] + '\t' + line_arr[0] + '\n'
+							res = res + f'{line_arr[0]}: {line_arr[1]}\n'
+				
+				with open(out_file_path, 'w', encoding='utf-8') as o:
+					o.write(res)
+
+
+current_dir = Path.cwd()
+src_dir = current_dir / 'src'
+out_dir = current_dir / 'out'
+
+# 如果存在输出文件，先删除
+if os.path.exists(out_dir):
+	shutil.rmtree(out_dir)
+os.mkdir(out_dir)
+
+convert(src_dir, out_dir, 'js6.dict.yaml')
