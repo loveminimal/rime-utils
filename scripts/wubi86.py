cd8274e13539351a3c8734519c353491074eb895
jack <loveminimal@163.com>
2024-12-26 17:17:37 +0800

    feat: ok wubi86

--- scripts/wubi86.py
+++ scripts/wubi86.py
@@ -4,63 +4,62 @@ import re
 import shutil
 from pathlib import Path
 from header import get_header
-# from data.wubi86yd import get_wubi86yd
 from data.pinyin8105 import pinyin8105
 
 
-def convert(SRC_DIR, OUT_DIR, FILE_ENDSWITH_FILETER, OUT_MODE):
+def convert(SRC_DIR, OUT_DIR, FILE_ENDSWITH_FILETER, MULTIFILE_OUT_MODE):
 	# 遍历源文件夹文件，处理
-	num = 0
+	dict_num = 0
 	res_dict = {}
 	code_list = []
 	lines_total = []
+
 	for file_path in SRC_DIR.iterdir():
 		if file_path.is_file() and file_path.name.endswith(FILE_ENDSWITH_FILETER):
-			num = num + 1
-			print('已加载第 %d 份码表 » %s' % (num, file_path))
-
-			# with open(OUT_DIR / f'wubi86.dict.yaml', 'w', encoding='utf-8') as o:
-			# 	o.write(get_header(file_path.name))
+			dict_num = dict_num + 1
+			print('已加载第 %d 份码表 » %s' % (dict_num, file_path))
 
 			with open(file_path, 'r', encoding='utf-8') as f:
 				lines = f.readlines()
 				lines_total.extend(lines)
 
+	# 设定最大统计字长列表 - 15个字
 	word_len_list = list(range(15))
+
 	for word_len in word_len_list:
 		res = ''
-		# res_dict = {}
-		# code_list = []
 		for line in lines_total:
+			# 从首个包含 8105 单字开头的行开始处理
 			if line[0] in pinyin8105:
-					
 				line_list = re.split(r'\t+',line.strip())
 				word = line_list[0]
 				code = line_list[1]
 				weight = line_list[2] if len(line_list) > 2 else '0'
 
+				# 按字长顺序过滤依次处理 1, 2, 3, 4 ...
 				if len(word) == word_len and all(w in pinyin8105 for w in word):
+					# 仅处理已合成词典中 不存在 或 已存在但编码不同的字词
 					if word not in res_dict or code not in res_dict[word]:
 						res = res + f'{word}\t{code}\t{weight}\n'
-						# res_dict[word] = code
 						code_list.append(code)
 						res_dict[word] = code_list
-		# print(res_dict)
+
 		if len(res.strip()) > 0:
-			OUT_MODE = int(OUT_MODE)
-			if OUT_MODE == 1:
+			MULTIFILE_OUT_MODE = int(MULTIFILE_OUT_MODE)
+			# 按字长生成多个文件
+			if MULTIFILE_OUT_MODE == 1:
 				with open(OUT_DIR / f'wubi86_{word_len}.dict.yaml', 'a', encoding='utf-8') as o:
 					print('» 已合并处理生成 %s 字文件' % word_len)
 					o.write(get_header(f'wubi86_{word_len}.dict.yaml'))
 					o.write(res)
-			elif OUT_MODE == 0:
+			# 统一生成在单个文件
+			elif MULTIFILE_OUT_MODE == 0:
 				with open(OUT_DIR / f'wubi86.dict.yaml', 'a', encoding='utf-8') as o:
 					print('» 已合并处理生成 %s 字词语' % word_len)
-					word_len == 1 and o.write(get_header(f'wubi86.dict.yaml'))	# 第一份源文件 且字长为1时 添加表头
+					word_len == 1 and o.write(get_header(f'wubi86.dict.yaml'))	# 仅字长为 1 时添加表头
 					o.write(res)
 
 if __name__ == '__main__':
-	# print(len(pinyin8105))
 	current_dir = Path.cwd()
 
 	src = 'src'
@@ -68,7 +67,8 @@ if __name__ == '__main__':
 	file_endswith_filter = '.dict.yaml'
 	multifile_out_mode = 0
 
-	# 假设命令行输入: python script.py -m value1 -n value2
+	# 命令行输入选项
+	# ... py scripts/wubi86.py [-i src] [-o out] [-f file_endswith_filter] [-m multifile_out_mode]
 	for i, arg in enumerate(sys.argv):
 		if arg == "-i":
 			src = sys.argv[i + 1]
@@ -80,8 +80,7 @@ if __name__ == '__main__':
 			multifile_out_mode = sys.argv[i + 1]
 
 	src_dir = current_dir / src				# 设置待处理的拼音词库文件夹
-	out_dir =  current_dir / out		    # 转换后输出的五笔词库文件夹
-
+	out_dir =  current_dir / out			# 转换后输出的五笔词库文件夹
 
 	# 如果存在输出文件，先删除
 	if out_dir.exists():
