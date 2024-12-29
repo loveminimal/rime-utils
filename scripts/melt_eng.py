f8e542ea5d109c409a1bb187da7eef69b42dd33c
jack <loveminimal@163.com>
2024-12-29 13:16:57 +0800

    fix: melt_eng set

--- scripts/melt_eng.py
+++ scripts/melt_eng.py
@@ -33,12 +33,20 @@ def convert(SRC_DIR, OUT_DIR, FILE_ENDSWITH_FILETER, MULTIFILE_OUT_MODE):
 
 	# 设定最大统计字长列表 - 15个字
 	word_len_list = list(range(60))
-
+	lines_list = []
 	for word_len in word_len_list:
 		res = ''
 		for line in lines_total:
 			# 从首个包含 8105 单字开头的行开始处理
-			if line[0] not in '&#-. ' and line.strip() not in ['name: en', 'version: "2024-12-27"','version: "2024-12-28"','sort: by_weight','name: en_ext','name: en_ext_yd','']:
+			# if line[0] not in '&#-. ' and line.strip() not in ['name: en', 'version: "2024-12-27"','version: "2024-12-28"','sort: by_weight','name: en_ext','name: en_ext_yd','']:
+			if line[0] not in '&#-. ' and line.strip() not in \
+				['name: en', \
+				'version: "2024-12-27"',\
+				'version: "2024-12-28"',\
+				'sort: by_weight',\
+				'name: en_ext',\
+				'name: en_ext_yd',\
+				'']:
 				# print(line)
 				line_list = re.split(r'\t+',line.strip())
 				if len(line_list) < 2:
@@ -51,25 +59,38 @@ def convert(SRC_DIR, OUT_DIR, FILE_ENDSWITH_FILETER, MULTIFILE_OUT_MODE):
 				# if len(word) == word_len and all(w in pinyin8105 for w in word):
 				if len(word) == word_len:
 					# 仅处理已合成词典中 不存在 或 已存在但编码不同的字词
-					if word not in res_dict or code not in res_dict[word]:
+					# if word not in res_dict or code not in res_dict[word]:
+					if True:
 						res = res + f'{word}\t{code}\t{weight}\n'
-						code_list.append(code)
-						res_dict[word] = code_list
+						# code_list.append(code)
+						# res_dict[word] = code_list
+						lines_list.append(f'{word}\t{code}\t{weight}\n')
 
-		if len(res.strip()) > 0:
-			MULTIFILE_OUT_MODE = int(MULTIFILE_OUT_MODE)
-			# 按字长生成多个文件
-			if MULTIFILE_OUT_MODE == 1:
-				with open(OUT_DIR / f'en_{word_len}.dict.yaml', 'a', encoding='utf-8') as o:
-					print('✅  » 已合并处理生成 %s 字文件' % word_len)
-					o.write(get_en_header(f'en_{word_len}.dict.yaml'))
-					o.write(res)
-			# 统一生成在单个文件
-			elif MULTIFILE_OUT_MODE == 0:
-				with open(OUT_DIR / f'en.dict.yaml', 'a', encoding='utf-8') as o:
-					print('✅  » 已合并处理生成 %s 字词语' % word_len)
-					word_len == 1 and o.write(get_en_header(f'en.dict.yaml'))	# 仅字长为 1 时添加表头
-					o.write(res)
+		# if len(res.strip()) > 0:
+		# 	MULTIFILE_OUT_MODE = int(MULTIFILE_OUT_MODE)
+		# 	# 按字长生成多个文件
+		# 	if MULTIFILE_OUT_MODE == 1:
+		# 		with open(OUT_DIR / f'en_{word_len}.dict.yaml', 'a', encoding='utf-8') as o:
+		# 			print('✅  » 已合并处理生成 %s 字文件' % word_len)
+		# 			o.write(get_en_header(f'en_{word_len}.dict.yaml'))
+		# 			o.write(res)
+		# 	# 统一生成在单个文件
+		# 	elif MULTIFILE_OUT_MODE == 0:
+		# 		with open(OUT_DIR / f'en.dict.yaml', 'a', encoding='utf-8') as o:
+		# 			print('✅  » 已合并处理生成 %s 字词语' % word_len)
+		# 			word_len == 1 and o.write(get_en_header(f'en.dict.yaml'))	# 仅字长为 1 时添加表头
+		# 			o.write(res)
+
+	# lines_list = list(set(lines_list))
+	# print(len(lines_list))
+	unique_list = []
+	[unique_list.append(item) for item in lines_list if item not in unique_list]
+	# print(len(unique_list))
+	result = ''.join(unique_list)
+	with open(OUT_DIR / f'en.dict.yaml', 'a', encoding='utf-8') as o:
+		print(f'✅  » 已合并排序去重英文码表 - 共 {len(lines_list)} » {len(unique_list)} 条')
+		o.write(get_en_header(f'en.dict.yaml'))	# 仅字长为 1 时添加表头
+		o.write(result)
 
 if __name__ == '__main__':
 	current_dir = Path.cwd()
